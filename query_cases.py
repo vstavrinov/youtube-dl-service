30310d12165166e3f101edc24f4318321a2c0908
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2021-05-20 19:38:38 +0300

    add tests

---
+++ query_cases.py
@@ -0,0 +1,30 @@
+import os
+import unittest
+
+if 'test_url' in os.environ:
+    url = os.environ['test_url']
+else:
+    url = 'youtube.com/c/ABCNews/live'
+sample = '474011'
+
+
+class TestCases(unittest.TestCase):
+
+    def probe(self, args, count):
+        '''Make actual query to the service. Redefined in child class'''
+        pass
+
+    def test_video(self):
+        '''Get video stream by implicit parameter'''
+        args = url
+        self.assertTrue(self.probe(args, len(sample)//2).hex() == sample)
+
+    def test_formats(self):
+        '''Check for available formats (worst ... best)'''
+        args = url + '&list-formats'
+        self.assertTrue('Available formats for ' in self.probe(args, None).decode())
+
+    def test_help(self):
+        '''Pull out long help message'''
+        args = 'help'
+        self.assertTrue('Usage: youtube-dl ' in self.probe(args, None).decode())
---
+++ test_docker.py
@@ -0,0 +1,12 @@
+from urllib.request import urlopen
+import query_cases
+from time import sleep
+
+endpoint = 'http://localhost:8000?'
+
+
+class TestQuery(query_cases.TestCases):
+
+    def probe(self, args, count):
+        sleep(2)
+        return urlopen(endpoint + args).read(count)
